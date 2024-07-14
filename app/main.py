a25914a2e90d26a246e0b530ef76023520dcd521
khc5308 <78535114+khc5308@users.noreply.github.com>
2024-07-14 22:37:37 +0900

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Header, HTTPException
+from fastapi.responses import FileResponse
+from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+
+client = MongoClient("mongodb://par6:Haaaaa@hs.5-23.dev:3000")
+db = client['para_user']
+
+app = FastAPI()
+
+
+@app.get("/") #기본 url
+def home():
+    return FileResponse("./Front/index.html")
+
+@app.get("/login")
+def 로그인():
+    return FileResponse("./Front/signup.html")
+
+@app.post("/signup_submit")
+async def 회원가입(id:str = Header(None),pw:str=Header(None)):
+    
+    
+    # MongoDB에 문서 추가
+    result = client['para_user'].insert_one({"id": id, "password": pw})
+    
+    # # 삽입된 문서의 ID 반환
+    inserted_id = str(result.inserted_id)
+    
+    return {"message": "회원가입이 완료되었습니다.", "inserted_id": inserted_id}
+
+
+    #return user_document
